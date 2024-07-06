610b8da21e6af96f06f996af7f3b3185955c8727
Hadi Jahanshahi <hadij@yorku.ca>
2024-07-05 22:27:55 -0400

    2

--- ann_homework_solution.py
+++ ann_homework_solution.py
@@ -22,18 +22,26 @@ X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
 # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
 labelencoder_X_1 = LabelEncoder()
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
-X = onehotencoder.fit_transform(X).toarray()
-X = X[:, 1:]
+
+# Applying OneHotEncoder to the categorical feature
+ct = ColumnTransformer(
+    transformers=[('encoder', OneHotEncoder(), [1])],
+    remainder='passthrough'
+)
+X = np.array(ct.fit_transform(X), dtype=float)
+X = X[:, 1:]  # Avoid the dummy variable trap by dropping the first column
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -52,19 +60,19 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu', input_dim=11))
 
 # Adding the second hidden layer
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
 
 # Adding the output layer
-classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
 
 # Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+classifier.fit(X_train, y_train, batch_size=10, epochs=100)
 
 # Part 3 - Making predictions and evaluating the model
 
@@ -73,7 +81,8 @@ y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 
 # Predicting a single new observation
-"""Predict if the customer with the following informations will leave the bank:
+"""
+Predict if the customer with the following informations will leave the bank:
 Geography: France
 Credit Score: 600
 Gender: Male
@@ -83,10 +92,14 @@ Balance: 60000
 Number of Products: 2
 Has Credit Card: Yes
 Is Active Member: Yes
-Estimated Salary: 50000"""
+Estimated Salary: 50000
+"""
 new_prediction = classifier.predict(sc.transform(np.array([[0.0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
 new_prediction = (new_prediction > 0.5)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+print("Confusion Matrix:\n", cm)
+print("New Prediction (will the customer leave?):", new_prediction)
