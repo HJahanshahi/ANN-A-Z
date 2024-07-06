94deec55230e98f9a3762ac254d01db9464887d4
Hadi Jahanshahi <hadij@yorku.ca>
2024-07-05 22:40:55 -0400

    2

--- ann_homework_solution.py
+++ ann_homework_solution.py
@@ -1,3 +1,5 @@
+print("Starting script...")
+
 # Artificial Neural Network
 
 # Installing Theano
@@ -11,16 +13,18 @@
 
 # Part 1 - Data Preprocessing
 
-# Importing the libraries
+print("Importing libraries...")
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+print("Importing dataset...")
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
+print("Encoding categorical data...")
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder
 from sklearn.compose import ColumnTransformer
@@ -39,22 +43,24 @@ ct = ColumnTransformer(
 X = np.array(ct.fit_transform(X), dtype=float)
 X = X[:, 1:]  # Avoid the dummy variable trap by dropping the first column
 
+print("Splitting the dataset...")
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+print("Feature Scaling...")
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+print("Building the ANN...")
 # Part 2 - Now let's make the ANN!
 
 # Importing the Keras libraries and packages
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
 # Initialising the ANN
 classifier = Sequential()
@@ -68,18 +74,22 @@ classifier.add(Dense(units=6, kernel_initializer='uniform', activation='relu'))
 # Adding the output layer
 classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
 
+print("Compiling the ANN...")
 # Compiling the ANN
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+print("Fitting the ANN to the Training set...")
 # Fitting the ANN to the Training set
 classifier.fit(X_train, y_train, batch_size=10, epochs=100)
 
+print("Making predictions...")
 # Part 3 - Making predictions and evaluating the model
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 
+print("Predicting a single new observation...")
 # Predicting a single new observation
 """
 Predict if the customer with the following informations will leave the bank:
@@ -97,6 +107,7 @@ Estimated Salary: 50000
 new_prediction = classifier.predict(sc.transform(np.array([[0.0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])))
 new_prediction = (new_prediction > 0.5)
 
+print("Making the Confusion Matrix...")
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
